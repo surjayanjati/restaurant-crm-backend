362e2e68c625156f7787ab7eca2e534011090def
surjayanjati <surjojati@gmail.com>
2023-02-28 20:27:17 +0530

    Admin -Pnl Food Curd operation Done Commit

--- controller/adminPanelFoodCurdController.js
+++ controller/adminPanelFoodCurdController.js
@@ -40,8 +40,90 @@ exports.addFoodItem=async(req,res)=>{
    
     }else res.send({msg:"You Don't Have The Access To This",success:false,status:401});    
     } catch (error) {
-        
+        res.send({msg:"Unable To The New Food",success:false,status:500});
     }   
 };
 
 /// Controller For Super-Admin/Admin So They Can Delete The Food---------------------------------------------------------------->
+exports.deleteFoodItem=async(req,res)=>{
+    try {
+        // Object Destrcturing For Getting values from middleware_________________________/
+        const {role,companyId,companyName,companyEmail}=req.details;
+        if(role==="Super-Admin" || role==="Admin"){
+        // Object Destrcuring Getting The Values From The Req Body________________________/
+        const foodId=req.body.foodId;    
+        if( foodId!==""){
+        const deleteResult=await foodCollections.updateOne({companyId:companyId},{$pull:{foodsArray:{foodId:foodId}}});
+        if(deleteResult.acknowledged===true){
+        res.send({msg:"Food Item Has Been Deleted",success:true,status:200});
+        }else res.send({msg:"Unable To Delete The Food",success:false,status:500}); 
+        }else res.send({msg:"Unable To Delete The Food",success:false,status:500}); 
+       
+        }else res.send({msg:"You Don't Have The Access To This",success:false,status:401});    
+        } catch (error) {
+            res.send({msg:"Unable To The New Food",success:false,status:500});
+        }   
+};
+
+/// Controller For Super-Admin/Admin so They Can Update The Food Price ---------------------------------------------------------------->
+exports.updateFoodPrice=async(req,res)=>{
+    try {
+        // Object Destrcturing For Getting values from middleware_________________________/
+        const {role,companyId,companyName,companyEmail}=req.details;
+        if(role==="Super-Admin" || role==="Admin"){
+        // Object Destrcuring Getting The Values From The Req Body________________________/
+        const {foodId,foodPrice}=req.body;    
+        if( foodId!==""  && foodPrice!==""){
+        const deleteResult=await foodCollections.updateOne({companyId:companyId,"foodsArray.foodId":foodId},{$set:{"foodsArray.$.foodPrice":foodPrice}});
+        if(deleteResult.acknowledged===true){
+        res.send({msg:"Food Item Price Has Been Updated",success:true,status:200});
+        }else res.send({msg:"Unable To Update The Food Price",success:false,status:500}); 
+        }else res.send({msg:"Kindly Put All The Details",success:false,status:500}); 
+       
+        }else res.send({msg:"You Don't Have The Access To This",success:false,status:401});    
+        } catch (error) {
+            res.send({msg:"Unable To Update The Food Price",success:false,status:500});
+        }   
+};
+
+/// Controller For Super-Admin/Admin When They want To Search The Name on Search Bar-------------------------------------------------->
+exports.searchFoodItem=async(req,res)=>{
+    try {
+        // Object Destrcturing For Getting values from middleware_________________________/
+        const {role,companyId,companyName,companyEmail}=req.details;
+        if(role==="Super-Admin" || role==="Admin"){
+        // Object Destrcuring Getting The Values From The Req Body________________________/
+        const foodName=req.body.foodName;    
+        if( foodName!==""){
+        const searchResult=await foodCollections.findOne({companyId:companyId,"foodsArray.foodName":foodName});
+        if(searchResult!==null){
+        res.send({msg:"Food Item Price Has Been Updated",success:true,status:200,data:searchResult});
+        }else res.send({msg:"No Food Item Found",success:false,status:400}); 
+        }else res.send({msg:"Kindly Enter The Food Name",success:false,status:500}); 
+       
+        }else res.send({msg:"You Don't Have The Access To This",success:false,status:401});    
+        } catch (error) {
+            res.send({msg:"No Food Item Found",success:false,status:500});
+        }   
+};
+
+
+/// Controller For Super-Admin/Admin When They want To Fetch All The Food Details ----------------------------------------------------->
+exports.fetchFoodItem=async(req,res)=>{
+    try {
+        // Object Destrcturing For Getting values from middleware_________________________/
+        const {role,companyId,companyName,companyEmail}=req.details;
+        if(role==="Super-Admin" || role==="Admin"){
+     
+       
+        const searchResult=await foodCollections.findOne({companyId:companyId});
+        if(searchResult!==null){
+        res.send({msg:"Food Items Fetching Successfull",success:true,status:200,data:searchResult});
+        }else res.send({msg:"No Data Found",success:false,status:400}); 
+      
+       
+        }else res.send({msg:"You Don't Have The Access To This",success:false,status:401});    
+        } catch (error) {
+            res.send({msg:"No Data Found",success:false,status:500});
+        }   
+}
--- routes/adminPanelFoodCurdRoutes.js
+++ routes/adminPanelFoodCurdRoutes.js
@@ -8,7 +8,19 @@ const roleCheckAuth=require("../middleware/roleCheckAuth");
 module.exports=(app)=>{
 
 
-    /// Route For Adding The Food__________________________________/
-    app.post("/crm/api/v1/adminpanel/companies/addfood",roleCheckAuth.tokenCheck,roleCheckAuth.roleCheck,adminPanelfoodCurdController.addFoodItem);
+    /// Route For Adding The Food_____________________________________________/
+    app.post("/crm/api/v1/adminpanel/companies/addfoods",roleCheckAuth.tokenCheck,roleCheckAuth.roleCheck,adminPanelfoodCurdController.addFoodItem);
+    
+    /// Route For Deleting The Food__________________________________________/
+    app.delete("/crm/api/v1/adminpanel/companies/deletefoods",roleCheckAuth.tokenCheck,roleCheckAuth.roleCheck,adminPanelfoodCurdController.deleteFoodItem);
 
+    /// Route For Updating The Food Price__________________________________/
+    app.put("/crm/api/v1/adminpanel/companies/updatefoodprices",roleCheckAuth.tokenCheck,roleCheckAuth.roleCheck,adminPanelfoodCurdController.updateFoodPrice);
+
+    /// Route For Searching The Food Item_______________________________/
+    app.get("/crm/api/v1/adminpanel/companies/searchfooditems",roleCheckAuth.tokenCheck,roleCheckAuth.roleCheck,adminPanelfoodCurdController.searchFoodItem);
+
+    
+    /// Route For Fetching The  Food Item_______________________________/
+    app.get("/crm/api/v1/adminpanel/companies/fetchfooditems",roleCheckAuth.tokenCheck,roleCheckAuth.roleCheck,adminPanelfoodCurdController.fetchFoodItem);
 }
